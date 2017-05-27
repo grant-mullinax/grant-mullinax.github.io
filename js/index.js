52491592fe4e8bc2b8eea7130b31c1f6517745f6
mullinaxgr2 <mullinaxgr@gmail.com>
2017-05-27 15:01:58 -0400

    partial support

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
-let context = ">",
-    text = "",
-    previousText = "",
-    index = -1,
+let previousText = "",
     logIndex = 0,
+    blinking,
+    context,
+    text,
+    index,
+    mobile;
+
+if (typeof window.orientation === 'undefined') {
     blinking = false;
+    context = ">";
+    text = "";
+    index = -1;
+    mobile = false;
+}else{
+    mobile = true;
+}
 
 function scrollToBottom() {
     $("#bottom")[0].scrollIntoView();
@@ -20,13 +31,8 @@ function newLogSection(){
     scrollToBottom();
 }
 
-function logLink(text, url){
-    $("#log").append('<a href="'+url+'" target="_blank">'+text+'</a>');
-    newLogSection();
-}
-
-function logImg(img){
-    $("#log").append('<img src="img/'+img+'" alt="president of the united states">');
+function appendToLogs(html){
+    $("#log").append(html);
     newLogSection();
 }
 
@@ -46,16 +52,16 @@ class Command{
         this.help = desc;
     }
 
-    run(s){
-        this.func(s[1],s[2],s[3],s[4]);
-        //don't hate the player, hate the game. you shouldn't need more than 4 arguments, right?
+    run(args){
+        this.func(args);
     }
 }
 
 let commands =
     {
         "help": new Command(
-            function (command) {
+            function (args) {
+                let command = args[0];
                 if (typeof command !== "undefined") {
                     if (typeof commands[command] !== "undefined"){
                         log(command + " : " + commands[command].help);
@@ -64,7 +70,7 @@ let commands =
                     }
                 }else{
                     log("these are all the commands:");
-                    for(command in commands) this.func(command);
+                    for(command in commands) this.func([command]);
                 }
             },"provides info about a command"
         ),
@@ -76,96 +82,118 @@ let commands =
         ),
 
         "echo": new Command(
-            function (reply) {
-                log(reply);
+            function (args) {
+                let text = "";
+                args.forEach(function (arg,i) {
+                    text+=arg+" ";
+                });
+                log(text);
             },"it echoes what you say!"
+        ),
+
+        "clear": new Command(
+            function () {
+                $("#log").html('<pre id="log_section_0"></pre>');
+                logIndex = 0;
+            },"clear the logs"
         ),
 
         "github": new Command(
             function () {
-                logLink("github","https://github.com/grant-mullinax")
+                appendToLogs('<a href="https://github.com/grant-mullinax" target="_blank"><img src="img/github-logo.png" alt="github" width="100" height="100"></a>')
             },"displays a link to my github"
         ),
 
         "google": new Command(
             function () {
-                logLink("google","https://www.google.com")
+                appendToLogs('<a href="https://www.google.com" target="_blank">Google</a>')
             },"displays a link to google"
         ),
 
         "trump": new Command(
             function () {
-                logImg("trump.jpg")
+                appendToLogs('<img src="img/trump.jpg" alt="president of the united states">');
             },"displays a picture of the president of the united states"
         ),
 
-        "hire-grant": new Command(
+        "hire-me": new Command(
             function () {
                 log("youve done it!");
             },"claim your destiny"
         )
     };
 
-$(document).keydown(function(e) {
-    console.log(e.keyCode);
-    switch(e.keyCode){
-        case 13://enter
-            if (text!=="") {
-                log(">"+text);
-            }else{
-                text = previousText;
-            }
+function executeCommand(text){
+    if (text !== "") {
+        log(">" + text);
+    } else {
+        text = previousText;
+    }
 
-            let split = text.split(" ");
-            let commandName = split[0].toLowerCase();
-            if (typeof commands[commandName] !== "undefined")
-                commands[commandName].run(split);
+    let split = text.split(" ");
+    let commandName = split[0].toLowerCase();
+    if (typeof commands[commandName] !== "undefined") {
+        split.shift();
+        commands[commandName].run(split);
+    }
 
-            previousText = text;
+    previousText = text;
+}
 
-            text = "";
-            index = -1;
-            break;
-        case 8://backspace
-            if(index>=0){
-                text = text.slice(0, index) + text.slice(index+1);
-                index--;
-            }
-            break;
-        case 46://delete
-            text = text.slice(0, index+1) + text.slice(index+2);
-            break;
-        case 37: //left
-            console.log('left');
-            if (index>=0)
-                index--;
-            break;
-        case 39: //right
-            if (index<text.length-1)
-                index++;
-            break;
-        default:
-            if (e.key.length===1) {
-                index++;
-                if (index===text.length){
-                    text+=e.key;
-                }else{
-                    text = text.slice(0, index) + e.key + text.slice(index);
+if (!mobile) {
+    $(document).keydown(function (e) { //desktop
+        switch (e.keyCode) {
+            case 13://enter
+                executeCommand(text);
+
+                text = "";
+                index = -1;
+                break;
+            case 8://backspace
+                if (index >= 0) {
+                    text = text.slice(0, index) + text.slice(index + 1);
+                    index--;
                 }
-            }
-    }
-    updateCmdText();
-});
-
-setInterval(
-    function(){
-        blinking=!blinking;
+                break;
+            case 46://delete
+                text = text.slice(0, index + 1) + text.slice(index + 2);
+                break;
+            case 37: //left
+                console.log('left');
+                if (index >= 0)
+                    index--;
+                break;
+            case 39: //right
+                if (index < text.length - 1)
+                    index++;
+                break;
+            default:
+                if (e.key.length === 1) {
+                    index++;
+                    if (index === text.length) {
+                        text += e.key;
+                    } else {
+                        text = text.slice(0, index) + e.key + text.slice(index);
+                    }
+                }
+        }
         updateCmdText();
-    }
-    , 500
-);
+    });
 
-$('#retro_overlay').on('dragstart', function(event) { event.preventDefault(); });
+    setInterval(
+        function(){
+            blinking=!blinking;
+            updateCmdText();
+        }
+        , 500
+    );
+}
+
+function mobileCommand(){
+    let cmdline = $("#mobile_cmdline");
+    executeCommand(cmdline.val());
+    cmdline.val('');
+}
 
 commands.help.run('');
-log('');
+log('1.0');
